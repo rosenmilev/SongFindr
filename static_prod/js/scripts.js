aee55323da22e66df0ca4cb13745757dc1132fb9
traxx900 <rosenmilev@gmail.com>
2023-03-24 01:49:02 +0200

    Configure static files for production

---
+++ static_prod/js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    function highlightWord() {
+        $(".song-result").each(function () {
+            const word = $("#id_word").val();
+            const regex = new RegExp(`(${word})`, 'gi');
+            const resultHTML = $(this).children('span').html().replace(regex, '<span class="highlight">$1</span>');
+            $(this).children('span').html(resultHTML);
+        });
+    }
+
+    function fadeInResults() {
+        setTimeout(function () {
+            $(".song-result").addClass("visible");
+        }, 100);
+    }
+
+    highlightWord();
+    fadeInResults();
+
+    $(".youtube-search").on("click", function () {
+        const searchQuery = $(this).data("search");
+        const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchQuery)}`;
+        window.open(url, "_blank");
+    });
+
+    // Hide the "New search" button by default
+    $("#new-search").hide();
+
+    // Show the "New search" button when the search button is clicked
+    $("form.search-box").on("submit", function (event) {
+        event.preventDefault();
+        $("#new-search").show();
+        // The code below simulates the submission of the form and reloads the page with the search results
+        this.submit();
+    });
+
+    $("#new-search").on("click", function () {
+        location.reload();
+    });
+});
+document.querySelector('.search-title').addEventListener('click', function () {
+    window.location.reload();
+});
